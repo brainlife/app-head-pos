2ba65b53d500a2ba59114036047ec18476897ae6
AuroreBussalb <aurore.bussalb@gmail.com>
2021-02-12 16:52:04 +0100

    update app to run on Brainlife

--- head-pos.py
+++ head-pos.py
@@ -1,20 +1,27 @@
 import mne
 import json
 
-
-# Test version
-print(mne.__version__)
+# Generate a json.product to display messages on Brainlife UI
+dict_json_product = {'brainlife': []}
 
 # Load inputs from config.json
 with open('config.json') as config_json:
     config = json.load(config_json)
 
 # Get cHPI
-data_file = str(config.pop('input_raw'))
+data_file = str(config.pop('fif'))
 raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)  # raw file must contain cHPI info
-chpi_amplitudes = mne.chpi.compute_chpi_amplitudes(raw, **config['params_chpi_amplitudes'])
-chpi_locs = mne.chpi.compute_chpi_locs(raw.info, chpi_amplitudes, **config['params_chpi_locs'])
-head_pos = mne.chpi.compute_head_pos(raw.info, chpi_locs, **config['params_head_pos'])
+chpi_amplitudes = mne.chpi.compute_chpi_amplitudes(raw)
+chpi_locs = mne.chpi.compute_chpi_locs(raw.info, chpi_amplitudes)
+head_pos = mne.chpi.compute_head_pos(raw.info, chpi_locs)
 
 # Save file
-mne.chpi.write_head_pos(config['output'], head_pos)
+mne.chpi.write_head_pos("out_dir/head_pos.pos", head_pos)
+
+# Success message in product.json
+dict_json_product['brainlife'].append({'type': 'success',
+                                       'msg': 'head position file was written successfully.'})
+
+# Save the dict_json_product in a json file
+with open('product.json', 'w') as outfile:
+    json.dump(dict_json_product, outfile)
